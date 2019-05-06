0e1faa656291307939938f5b9d846e85782a5207
weilaishijieliu <927794225@qq.com>
2019-05-06 16:24:59 +0800

    第2次

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -6,8 +6,12 @@ class TestLogin:
 
     def test_login2(self):
         print("test_login2")
-        assert 1
+        assert 0
 
     def test_login3(self):
         print("test_login3")
+        assert 0
+
+    def test_login4(self):
+        print("test_login4")
         assert 1
